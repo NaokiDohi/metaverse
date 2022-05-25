77df7cb86ec705a53ed6debcc1fc28c8f393c8e2
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-05-25 14:44:07 +0900

    Generating a flat space in the metaverse

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // Declation of a new scene with Three.js
 const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xbfd1e5);
+scene.background = new THREE.Color(0xbfd1e5);
 
 // Camera and renserer configuration
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -10,17 +10,25 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 // Setting the sence lights
-const ambient_light = new THREE.AmbientLight(0x404040);
-const direction_light = new THREE.DirectionalLight(0x00ff00, 1);
+const ambient_light = new THREE.AmbientLight(0xbda355);
+const direction_light = new THREE.DirectionalLight(0xffffff, 1);
 ambient_light.add(direction_light);
 scene.add(ambient_light);
+
+// Setting up a flat space of the Metaverse
+const geometry_space = new THREE.BoxGeometry(100, 0.2, 50);
+const material_space = new THREE.MeshPhongMaterial({color: 0xffffff});
+const space = new THREE.Mesh(geometry_space, material_space);
+scene.add(space);
 
 // Cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
 camera.position.z = 5;
+camera.position.set(10, 5, 40);
 
 function animate() {
     cube.rotation.x += 0.05;
